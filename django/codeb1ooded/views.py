3de2daf6b0970751b1ba40ee0f1f6b608099e0e1
Megha <meghaaggarwal493@gmail.com>
2018-10-07 16:23:40 +0530

    Added achievements from db

--- django/codeb1ooded/views.py
+++ django/codeb1ooded/views.py
@@ -8,6 +8,7 @@ from database.models import *
 
 def createHomePage(request):
     all_achievements = []
+    all_projects = []
     achievements = Achievements.objects.all().order_by('-ACHIEVEMENT_ID')
     for ach in achievements:
         achievement = {
@@ -17,4 +18,16 @@ def createHomePage(request):
                 'issuer' : ach.ISSUER
         }
         all_achievements.append(achievement)
-    return render(request,'index.html', {'achievements': all_achievements})
+    projects = Project.objects.all().order_by('-PROJECT_ID')
+    iterator = 0
+    for pro in projects:
+        project = {
+                'iterator': iterator,
+                'title' : pro.TITLE,
+                'star_link' : pro.STAR_LINK,
+                'fa_icon' : pro.FA_ICON,
+                'description' : pro.DESCRIPTION
+        }
+        iterator += 1
+        all_projects.append(project)
+    return render(request,'index.html', {'achievements': all_achievements, 'projects': all_projects})
